20283b6903cdd5d7e7d25ec880748c3e8d8a5947
Razeb Enyi <enyirazeb@gmail.com>
2021-02-18 06:14:12 +0100

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -80,7 +80,7 @@
           </form>
         </div>
         <div
-          class="col-11 col-sm-9 col-md-7 col-lg-7 col-xl-7 text-center p-0 mt-3 mb-2"
+          class="col-11 col-sm-9 col-md-6 col-lg-6 col-xl-7 text-center p-0 mt-3 mb-2"
         >
           <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
             <h2 id="heading">Order Confirmation</h2>
